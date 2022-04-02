0047825676f740aaf6a0074d91e12be919198300
Juzi <Juzi@wwbjw.com>
2022-04-02 08:47:35 +0800

    push commit

--- src/main/java/com/atguigu/git/Git/GitTest.java
+++ src/main/java/com/atguigu/git/Git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("master text!");
         System.out.println("hot-fix text!");
+        System.out.println("push text!");
     }
 }
